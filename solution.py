95f2b6a2a43e5ad9cafd3bc884a0b00955f5876a
accidentallydoesnotwork <46189714+accidentallydoesnotwork@users.noreply.github.com>
2019-05-14 11:54:08 +0700

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+
+
+def contains_banana(img):
+	"""
+	Change the contents of this function so it behaves correctly
+	"""
+	return -3.14159
+
+def crop_image(img, quadrant):
+	"""
+	Change the contents of this function so it behaves correctly
+	"""
+	return img
+
+def find_banana(img):
+	"""
+	Change the contents of this function so it behaves correctly
+	"""
+	return "None"
